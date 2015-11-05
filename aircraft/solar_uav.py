215fd6cd9233af0c92e99828b8a87d8eb2323505
Edward Burnell <eburn@mit.edu>
2015-11-05 12:46:01 -0500

    more experimental style changes

--- aircraft/solar_uav.py
+++ aircraft/solar_uav.py
@@ -1,50 +1,48 @@
 from gpkit import Variable, Model
+Var = Variable
 
 
 class SolarUAV(Model):
     """LLhale by whoburg, inspired by Drela"""
 
-    def setup(self, substitutions=None):
+    def setup(self):
         # Performance
-        P = Variable("P", units="W", label="flight power")
-        eta_p = Variable(r"\eta_p", value=0.75, label="motor+prop efficiency")
-        L15D = Variable(r"(C_L^{1.5}/C_D)", value=25, label="power parameter")
-        rho = Variable(r"\rho", units="kg/m^3", label="air density",
-                       value=('sweep', [0.238, 0.449]))
-        wl = Variable("(m/S)", units="kg/m^2", label="wing loading")
-        g = Variable("g", units="m/s^2", value=9.8)
+        P = Var("P", "W", "flight power")
+        eta_p = Var(r"\eta_p", 0.75, "-", "motor+prop efficiency")
+        L15D = Var(r"(C_L^{1.5}/C_D)", 25, "-", "power parameter")
+        rho = Var(r"\rho", ('sweep', [0.238, 0.449]), "kg/m^3", "air density")
+        wl = Var("(m/S)", "kg/m^2", "wing loading")
+        g = Var("g", 9.8, "m/s^2")
 
         # solar cells
-        Psolar = Variable("P_{solar}", units="W/m^2", value=200,
-                          label="solar cell output")
-        rho_solar = Variable(r"\rho_{solar}", units="kg/m^2", value=1.2,
-                             label="solar cell area density")
-        eta_daylight = Variable(r"\eta_{daylight}", value=0.4,
-                                label="winter daylight percentage")
-        eta_sun_angle = Variable(r"\eta_{sun angle}", value=0.5,
-                                 label="average utilization from low sun angles")
+        Psolar = Var("P_{solar}", 200, "W/m^2", "solar cell output")
+        rho_solar = Var(r"\rho_{solar}", 1.2, "kg/m^2",
+                        "solar cell area density")
+        eta_daylight = Var(r"\eta_{daylight}", 0.4, "-",
+                           "winter daylight percentage")
+        eta_sun_angle = Var(r"\eta_{sun angle}", 0.5, "-",
+                            "average utilization from low sun angles")
 
         # battery
-        eta_batt = Variable(r"\eta_{batt}", value=0.9,
-                            label="round-trip battery efficiency")
-        m_batt = Variable("m_{batt}", units="kg", label="battery mass")
-        h_batt = Variable("h_{batt}", units="MJ/kg", value=0.85,
-                          label="battery specific energy")
+        eta_batt = Var(r"\eta_{batt}", 0.9, "-",
+                       "round-trip battery efficiency")
+        m_batt = Var("m_{batt}", "kg", "battery mass")
+        h_batt = Var("h_{batt}", 0.85, "MJ/kg", "battery specific energy")
 
         # vehicle / airframe / weights
-        m_struct = Variable("m_{struct}", units="kg", label="airframe mass")
-        f_struct = Variable("f_{struct}", value=0.2,
-                            label="structural weight fraction, m_struct/m_tot")
-        S = Variable("S", units="m^2", label="wing area")
-        m_tot = Variable("m_{tot}", units="kg", label="airplane mass")
-        A = Variable("A", value=25, label="aspect ratio")
-        b = Variable("b", units="ft", label="span")
+        m_struct = Var("m_{struct}", "kg", "airframe mass")
+        f_struct = Var("f_{struct}", 0.2, "-",
+                       "structural weight fraction, m_struct/m_tot")
+        S = Var("S", "m^2", "wing area")
+        m_tot = Var("m_{tot}", "kg", "airplane mass")
+        A = Var("A", 25, "-", "aspect ratio")
+        b = Var("b", "ft", "span")
 
         # operations / requirements
-        m_pay = Variable("m_{pay}", units="kg", value=3, label="payload mass")
-        P_non_flight = Variable(r"P_{non flight}", units="W", value=20,
-                                label="non-flight power requirement")
-        t_batt = Variable("t_{batt}", value=12, units="hr", label="time on batt")
+        m_pay = Var("m_{pay}", 3, "kg", "payload mass")
+        P_non_flight = Var(r"P_{non flight}", 20, "W",
+                           "non-flight power requirement")
+        t_batt = Var("t_{batt}", 12, "hr", "time on battery")
 
         return Model(S,
                      [P/S >= 1/eta_p*(1/L15D)*(2/rho)**0.5*(wl*g)**1.5,
@@ -54,8 +52,7 @@ class SolarUAV(Model):
                       m_batt*h_batt >= (P + P_non_flight)*t_batt,
                       m_struct >= f_struct*m_tot,
                       wl*S >= m_tot,
-                      b**2 == S*A],
-                     substitutions=substitutions)
+                      b**2 == S*A])
 
 
 if __name__ == "__main__":
