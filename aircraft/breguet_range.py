984bd66566b04a5c3361526cc51fb53b3f22773d
pgkirsch <kirschen@mit.edu>
2016-02-08 11:58:00 -0500

    Typo fixes and minor style tweaks

--- aircraft/breguet_range.py
+++ aircraft/breguet_range.py
@@ -13,34 +13,36 @@ class BreguetRange(Model):
     """
     def setup(self):
 
-        TSFC_min = Var('TSFC_{min}', 0.307, "lb/lbf/hr", "Minimum TSFC")
-        MTOW = Var('MTOW', 10000, "lbf", "Max take off weight")
-        W_e = Var('W_{e}', 7000, "lbf", "Operating Empty Weight")
-        LoverD_max = Var('LoverD_{max}', 15, "-", "Maximum Lift to Drag Ratio")
-        V_max = Var('V_{max}', 420, "knots", "Maximum Velocity")
+        # Fixed Parameters
+        TSFC_min = Var('TSFC_{min}', 0.307, 'lb/lbf/hr', 'Minimum TSFC')
+        MTOW     = Var('MTOW', 10000, 'lbf', 'Max takeoff weight')
+        W_e      = Var('W_{e}', 7000, 'lbf', 'Operating Empty Weight')
+        LD_max   = Var('\\left(\\frac{L}{D}\\right)_{max}', 15, '-',
+                       'Maximum lift-to-drag ratio')
+        V_max    = Var('V_{max}', 420, 'knots', 'Maximum Velocity')
 
-        #Constants
-        g = Var('g', 9.81, "m/s^2", "gravity")
+        # Constants
+        g = Var('g', 9.81, 'm/s^2', 'Gravitational acceleration')
 
-        #Free Variables
-        R = Var('R', "nautical_miles", "range")
-        V = Var('V', "knots", "Velocity")
-        LoverD = Var('LoverD', "-", "life to drag ratio")
-        TSFC = Var('TSFC', "lb/lbf/hr", "thrust specific fuel consuption")
-        W_init = Var('W_{init}', "lbf", "initial weight")
-        W_fuel = Var('W_{fuel}', "lbf", "fuel weight")
-        z_bre = Var('z_{bre}', "-", "Breguet parameter")
-        t = Var('t', "hr", "time")
+        # Free Variables
+        R      = Var('R', 'nautical_miles', 'Range')
+        V      = Var('V', 'knots', 'Velocity')
+        LD     = Var('\\frac{L}{D}', '-', 'Lift-to-drag ratio')
+        TSFC   = Var('TSFC', 'lb/lbf/hr', 'Thrust specific fuel consumption')
+        W_init = Var('W_{init}', 'lbf', 'Initial gross weight')
+        W_fuel = Var('W_{fuel}', 'lbf', 'Fuel weight')
+        z_bre  = Var('z_{bre}', '-', 'Breguet parameter')
+        t      = Var('t', 'hr', 'Flight time')
 
-        # Set up Model Equations
+        # Model
         objective = 1/R  # Maximize range
         constraints = [W_init >= W_e + W_fuel,
                        W_init <= MTOW,
-                       LoverD <= LoverD_max,
+                       LD <= LD_max,
                        TSFC >= TSFC_min,
                        V <= V_max,
                        t >= R/V,
-                       z_bre >= t*TSFC*g/LoverD,
+                       z_bre >= t*TSFC*g/LD,
                        W_fuel/W_e >= te_exp_minus1(z_bre, nterm=3)
                       ]
         return objective, constraints
@@ -48,6 +50,5 @@ class BreguetRange(Model):
     def test(self):
         _ = self.solve()
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     BreguetRange().test()
