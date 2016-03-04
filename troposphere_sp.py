80ba9469932fdb12f81ad1fb2acc5675a1d225cb
pgkirsch <kirschen@mit.edu>
2016-03-03 22:10:23 -0500

    replaced signomial inequality with mono equality

--- troposphere_sp.py
+++ troposphere_sp.py
@@ -1,4 +1,4 @@
-from gpkit import Variable, Model, SignomialsEnabled, units, disable_units
+from gpkit import Variable, Model, SignomialsEnabled, units
 import numpy.testing as npt
 
 
@@ -7,9 +7,12 @@ class TroposphereSP(Model):
     Density as a function of altitude based on standard atmosphere model
 
     Required pressures: downward pressure on rho
-    Assumptions: only valid to the top of the troposphere (~20km)
+    Assumptions: only valid to the top of the troposphere (~11km)
 
-    source: https://en.wikipedia.org/wiki/Density_of_air#Altitude
+    Sources:
+    https://en.wikipedia.org/wiki/Density_of_air#Altitude
+    http://www.digitaldutch.com/atmoscalc/index.htm
+    http://www.engineeringtoolbox.com/standard-atmosphere-d_604.html
 
     Arguments
     ---------
@@ -41,11 +44,11 @@ class TroposphereSP(Model):
         with SignomialsEnabled():
             objective = rho  # minimize density
             constraints = [  # Model only valid up to top of the troposphere
-                             h <= 20000*units.m,
+                             h <= 11000*units.m,
                              # Temperature decreases with height at a rate of L
-                             T_sl >= T + L*h,
+                             T_sl <= T + L*h,
                              # Pressure-altitude relation and ideal gas law
-                             (p/p_sl)**(1/th) + L*h/T_sl >= 1,
+                             (p/p_sl)**(1/th) == T/T_sl,
                              rho == p*MoR/T]
         return objective, constraints
 
