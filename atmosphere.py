29bcc1d84771efe0a1639d01c8ac23d8b36af0fc
pgkirsch <kirschen@mit.edu>
2016-03-04 15:42:23 -0500

    universal gas constant --> specific gas constant

--- atmosphere.py
+++ atmosphere.py
@@ -23,14 +23,12 @@ class Atmosphere(Model):
         Acceleration due to gravity
     L : float  [K/m]
         Temperature lapse rate
-    M : float [kg/mol]
-        Molar mass of dry air
-    R : float  [J/(mol*K)]
-        Universal gas constant
+    R : float  [J/(kg*K)]
+        Specific gas constant (air)
     """
-    def setup(self, g=9.81, L=0.0065, M=0.0289644, R=8.31447):
-        th = (g*M)/(R*L)  # dimensionless
-        self.g, self.L, self.M, self.R, self.th = g, L, M, R, th
+    def setup(self, g=9.81, L=0.0065, R=287):
+        th = g/(R*L)  # dimensionless
+        self.g, self.L, self.R, self.th = g, L, R, th
 
         # Free variables
         h   = Variable('h', 'm', 'Altitude')
@@ -41,35 +39,39 @@ class Atmosphere(Model):
 
         # Constants
         C_1  = Variable("C_1", 1.458E-6, "kg/(m*s*K^0.5)",
-                       "Sutherland coefficient")
+                        "Sutherland coefficient")
         L    = Variable('L', L, 'K/m', 'Temperature lapse rate')
-        MoR  = Variable('(M/R)', M/R, 'kg*K/J', 'Air property')
-        p_sl = Variable('p_{sl}', 101325, 'Pa', 'Pressure at sea level')
-        T_sl = Variable('T_{sl}', 288.15, 'K', 'Temperature at sea level')
+        R    = Variable('R', R, 'J/(kg*K)', 'Specific gas constant (air)')
+        p_0  = Variable('p_0', 101325, 'Pa', 'Pressure at sea level')
+        T_0  = Variable('T_0', 288.15, 'K', 'Temperature at sea level')
         T_S  = Variable("T_S", 110.4, "K", "Sutherland Temperature")
 
         with SignomialsEnabled():
             objective = 1/rho  # minimize density
             constraints = [  # Model only valid up to top of the troposphere
                              h <= 11000*units.m,
+
                              # Temperature decreases with height at a rate of L
-                             T_sl <= T + L*h,
-                             # Pressure-altitude relation and ideal gas law
-                             (p/p_sl)**(1/th) == T/T_sl,
-                             rho == p*MoR/T,
+                             T_0 <= T + L*h,
+
+                             # Pressure-altitude relation
+                             (p/p_0)**(1/th) == T/T_0,
+ 
+                             # Ideal gas law
+                             rho == p/(R*T),
 
                              # Sutherland viscosity model
                              ((T+T_S).mono_approximation({T:288.15,
-                                                        T_S:T_S.value.magnitude}))
+                                                          T_S:T_S.value.magnitude}))
                              * mu == C_1 * T**1.5
                           ]
         return objective, constraints
 
     def test(self):
         sol = self.localsolve()
-        h, p, T, rho, p_sl, T_sl, L, MoR = map(sol, ("h p T \\rho p_{sl} "
-                                                     "T_{sl} L (M/R)").split())
-        npt.assert_almost_equal(T_sl, T + L*h, decimal=2)
+        h, p, T, rho, p_0, T_0, L, R = map(sol, ("h p T \\rho p_0 "
+                                                   "T_0 L R").split())
+        npt.assert_almost_equal(T_0, T + L*h, decimal=2)
 
 if __name__ == "__main__":
     Atmosphere().test()
