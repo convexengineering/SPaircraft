72a1513199692ff7e2958317edb3145d57ca1c8d
pgkirsch <kirschen@mit.edu>
2016-03-03 22:07:05 -0500

    uses mono approximation instead, removed SP model

--- sutherland.py
+++ sutherland.py
@@ -25,41 +25,9 @@ class Sutherland(Model):
         C_1 = Variable("C_1", C1, "kg/(m*s*K^0.5)",
                        "Sutherland coefficient")
 
-        objective = 1/mu
-        constraints = [(1 + (T_S/T)) * mu <= C_1 * T**0.5]
-
-        return objective, constraints
-
-    def test(self):
-        sol = self.solve()
-
-class SutherlandSP(Model):
-    """
-    Dynamic viscosity (mu) as a function of temperature
-
-    Assumptions:
-    Downward pressure on viscosity
-
-    References:
-    http://www-mdp.eng.cam.ac.uk/web/library/enginfo/aerothermal_dvd_only/aero/
-           atmos/atmos.html
-    http://www.cfd-online.com/Wiki/Sutherland's_law
-    """
-    def setup(self):
-        # Free variables
-        mu = Variable("mu", "kg/(m*s)", "Dynamic viscosity")
-        T  = Variable("T", "K", "Temperature")
-
-        # Constants
-        T_S   = Variable("T_S", TS, "K", "Sutherland Temperature")
-        C_1 = Variable("C_1", C1, "kg/(m*s*K^0.5)",
-                       "Sutherland coefficient")
-
         objective = mu
-        with SignomialsEnabled():
-            constraints = [(1 + (T_S/T)) * mu >= C_1 * T**0.5]
+        constraints = [((T+T_S).mono_approximation({T:288.15,
+                                                    T_S:T_S.value.magnitude}))
+                       * mu == C_1 * T**1.5]
 
         return objective, constraints
-
-    def test(self):
-        sol = self.localsolve()
