4aaf44a3d61b9c0a36ce53e6156b1d2a0a08e773
mjburton <mjburton@mit.edu>
2016-03-15 06:38:29 -0400

    maximized npv, not working

--- npv/npv.py
+++ npv/npv.py
@@ -1,15 +1,31 @@
 ### NPV MODEL ###
 
-from gpkit import Model, Variables, units
+from gpkit import Model, Variable, units, VectorVariable
 import gpkit
 import numpy as np
 
 class NPV(Model):
     def setup(self):
 
+        N = 3 # number of payments
         NPV = Variable('NPV', 'USD', 'net present value')
-        C = Variable('C', 'USD', 'cash flow')
-        C_r = Variable('C_r', 4e6, 'USD/year', 'cash flow rate')
-        r = Variable('r', 0.02 '1/year', 'interest rate')
+        PV = Variable('PV', 'USD', 'present value')
+        C = VectorVariable(N, 'C', 'USD', 'cash flow')
+        C_r = Variable('C_r', 4e6, 'USD', 'cash flow rate')
+        r = Variable('r', 0.02, '-', 'interest rate')
+        t = VectorVariable(N, 't', '-', 'time')
+       
+        constraints = [3*PV >= NPV, 
+                       C >= PV*(1 + r*t + r*t**2/2 ),
+                       t >= t.left + C/C_r]
 
+        cost = 1/NPV
 
+        return cost, constraints
+
+    def test(self):
+        seft.solve()
+
+if __name__ == "__main__":
+    M = NPV()
+    sol = M.solve()
