4e7865fc4984894f5042a5a719a045361663b535
pgkirsch <kirschen@mit.edu>
2016-03-17 18:15:12 -0400

    basic geometric constraint relating CG and tail

--- aircraft/htail.py
+++ aircraft/htail.py
@@ -1,11 +1,11 @@
 from gpkit import Variable, Model, SignomialsEnabled, units
+from gpkit.constraints.tight import TightConstraintSet as TCS
 from numpy import pi
 
 
 AR = Variable('AR')
 CLa = Variable('C_{L_\\alpha}')
 eta = Variable('\\eta')
-Sh = Variable('Sh')
 SM = Variable('S.M.')
 dCG = Variable('\\Delta CG')
 de_da = Variable('\\frac{de}{da}')
@@ -20,6 +20,7 @@ lh   = Variable('l_h', 'm')
 lfuse= Variable('l_{fuse}', 'm')
 Sh   = Variable('S_h', 'm^2')
 Sw   = Variable('S_w', 'm^2')
+xCG  = Variable('x_{CG}', 'm')
 xw   = Variable('x_w', 'm')
 W    = Variable('W', 'N')
 
@@ -33,7 +34,7 @@ constraints = [
                # Trim
                CLh*Sh*lh/(Sw*cma) >= Cmac + CLw*xw/cma,
 
-#               lh <= lfuse,
+               TCS([xCG + lh <= lfuse]),
 
                # DATCOM formula (TODO: add sweep, Mach)
 #               CLa * (2 + ((AR/eta)**2 + 4)**0.5) <= 2*pi*AR,
@@ -46,9 +47,9 @@ substitutions = {
                  'C_{L_h}': 0.5,
                  'C_{L_w}': 0.5,
                  '\\bar{c}': 5,
-#                 'l_{fuse}': 40,
-                 'l_h': 20,
+                 'l_{fuse}': 40,
                  'S_w': 100,
+                 'x_{CG}': 20,
                  'x_w': 2,
                 }
 
