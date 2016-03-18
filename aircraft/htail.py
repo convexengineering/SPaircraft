89fb92ffdc74ebebb576007069b79e1cd2d7fcaa
pgkirsch <kirschen@mit.edu>
2016-03-17 20:02:12 -0400

    DATCOM formula for CLah

--- aircraft/htail.py
+++ aircraft/htail.py
@@ -1,17 +1,12 @@
 from gpkit import Variable, Model, SignomialsEnabled, units
 from gpkit.constraints.tight import TightConstraintSet as TCS
-from numpy import pi
+from numpy import pi, tan
 
-
-AR = Variable('AR')
-CLa = Variable('C_{L_\\alpha}')
-eta = Variable('\\eta')
-SM = Variable('S.M.')
-dCG = Variable('\\Delta CG')
-de_da = Variable('\\frac{de}{da}')
-dcmf_dcl = Variable('\\frac{dCmf}{dCL}')
-
-
+alpha = Variable('\\alpha', '-')
+ARh  = Variable('AR_h', '-')
+amax = Variable('\\alpha_{max}', '-')
+eta  = Variable('\\eta', '-')
+CLah = Variable('C_{L_{ah}}', '-')
 CLh  = Variable('C_{L_h}', '-')
 CLw  = Variable('C_{L_w}', '-')
 cma  = Variable('\\bar{c}', 'm')
@@ -20,6 +15,7 @@ lh   = Variable('l_h', 'm')
 lfuse= Variable('l_{fuse}', 'm')
 Sh   = Variable('S_h', 'm^2')
 Sw   = Variable('S_w', 'm^2')
+tanLh= Variable('\\tan(\\Lambda_h)', '-')
 xCG  = Variable('x_{CG}', 'm')
 xw   = Variable('x_w', 'm')
 W    = Variable('W', 'N')
@@ -36,19 +32,25 @@ constraints = [
 
                TCS([xCG + lh <= lfuse]),
 
-               # DATCOM formula (TODO: add sweep, Mach)
-#               CLa * (2 + ((AR/eta)**2 + 4)**0.5) <= 2*pi*AR,
+               # DATCOM formula (Mach number makes it SP)
+               TCS([(ARh/eta)**2 * (1 + tanLh**2) + 8*pi*ARh/CLah
+                    <= (2*pi*ARh/CLah)**2]),
+               CLh == CLah*alpha,
+               alpha <= amax,
 
                W >= 100*Sh*units.Pa
               ]
 
 substitutions = {
+                 '\\alpha_{max}': 0.1, # (6 deg)
+                 'AR_h': 4,
+                 '\\eta': 0.97,
                  'C_{m_{ac}}': 0.1,
-                 'C_{L_h}': 0.5,
                  'C_{L_w}': 0.5,
                  '\\bar{c}': 5,
                  'l_{fuse}': 40,
-                 'S_w': 100,
+                 'S_w': 125,
+                 '\\tan(\\Lambda_h)': tan(30*pi/180),
                  'x_{CG}': 20,
                  'x_w': 2,
                 }
---
+++ aircraft/htail.sol
@@ -0,0 +1,44 @@
+Using solver 'mosek'
+Solving for 6 variables.
+Solving took 0.017 seconds.
+
+Cost
+----
+ 2673 [N] 
+
+Free Variables
+--------------
+   C_{L_h} : 0.3508         
+C_{L_{ah}} : 3.508          
+       S_h : 26.73   [m**2] 
+         W : 2673    [N]    
+    \alpha : 0.1            
+       l_h : 20      [m]    
+
+Constants
+---------
+           AR_h : 4              
+        C_{L_w} : 0.5            
+     C_{m_{ac}} : 0.1            
+            S_w : 125     [m**2] 
+   \alpha_{max} : 0.1            
+        \bar{c} : 5       [m]    
+           \eta : 0.97           
+\tan(\Lambda_h) : 0.5774         
+       l_{fuse} : 40      [m]    
+            x_w : 2       [m]    
+         x_{CG} : 20      [m]    
+
+Sensitivities
+-------------
+            S_w : 1       
+            x_w : 0.6667  
+        C_{L_w} : 0.6667  
+        \bar{c} : 0.3333  
+     C_{m_{ac}} : 0.3333  
+\tan(\Lambda_h) : 0.1532  
+           AR_h : -0.3872 
+           \eta : -0.6128 
+       l_{fuse} : -1      
+   \alpha_{max} : -1      
+
