664fe1ee639bee44ad429adac36089a278a6a066
pgkirsch <kirschen@mit.edu>
2016-03-21 12:03:57 -0400

    stability constraint: add fuselage stuff, make posynomial, remove fudged objective

--- aircraft/htail.py
+++ aircraft/htail.py
@@ -12,6 +12,7 @@ CLh  = Variable('C_{L_h}', '-')
 CLw  = Variable('C_{L_w}', '-')
 cma  = Variable('\\bar{c}', 'm')
 Cmac = Variable('C_{m_{ac}}', '-')
+Kf   = Variable('K_f', '-')
 lh   = Variable('l_h', 'm')
 lfuse= Variable('l_{fuse}', 'm')
 Sh   = Variable('S_h', 'm^2')
@@ -22,14 +23,16 @@ tanLh= Variable('\\tan(\\Lambda_h)', '-')
 xCG  = Variable('x_{CG}', 'm')
 xw   = Variable('x_w', 'm')
 W    = Variable('W', 'N')
+wf   = Variable('w_f', 'm')
 
 
-objective = W*SM
+objective = W
 
 with SignomialsEnabled():
     constraints = [
                    # Stability
-                   TCS([SM + xw/cma >= CLah*Sh*lh/(CLaw*Sw*cma)]), # [SP]
+                   TCS([SM + xw/cma + Kf*wf**2*lfuse/(CLaw*Sw*cma)
+                        <= CLah*Sh*lh/(CLaw*Sw*cma)]),
                    SM >= SMmin,
 
                    # Trim
@@ -54,14 +57,16 @@ substitutions = {
                  'C_{L_{aw}}': 2*pi,
                  'C_{L_w}': 0.5,
                  '\\bar{c}': 5,
+                 'K_f': 0.7,
                  'l_{fuse}': 40,
                  'S_w': 125,
                  'S.M._{min}': 0.05,
                  '\\tan(\\Lambda_h)': tan(30*pi/180),
                  'x_{CG}': 20,
                  'x_w': 2,
+                 'w_f': 6,
                 }
 
 m = Model(objective, constraints, substitutions)
 
-m.localsolve()
+m.solve()
--- aircraft/htail.sol
+++ aircraft/htail.sol
@@ -1,19 +1,20 @@
-Beginning signomial solve.
-Solving took 6 GP solves and 0.31 seconds.
+Using solver 'mosek'
+Solving for 7 variables.
+Solving took 0.0167 seconds.
 
 Cost
 ----
- 207 [N] 
+ 3956 [N] 
 
 Free Variables
 --------------
-   C_{L_h} : 0.3508          
-C_{L_{ah}} : 3.508           
-      S.M. : 0.07746         
-       S_h : 26.73    [m**2] 
-         W : 2673     [N]    
-    \alpha : 0.1             
-       l_h : 20       [m]    
+   C_{L_h} : 0.3508         
+C_{L_{ah}} : 3.508          
+      S.M. : 0.05           
+       S_h : 39.56   [m**2] 
+         W : 3956    [N]    
+    \alpha : 0.1            
+       l_h : 20      [m]    
 
 Constants
 ---------
@@ -21,6 +22,7 @@ Constants
         C_{L_w} : 0.5            
      C_{L_{aw}} : 6.283          
      C_{m_{ac}} : 0.1            
+            K_f : 0.7            
      S.M._{min} : 0.05           
             S_w : 125     [m**2] 
    \alpha_{max} : 0.1            
@@ -28,20 +30,21 @@ Constants
            \eta : 0.97           
 \tan(\Lambda_h) : 0.5774         
        l_{fuse} : 40      [m]    
+            w_f : 6       [m]    
             x_w : 2       [m]    
          x_{CG} : 20      [m]    
 
 Sensitivities
 -------------
-        C_{L_w} : 4.776   
-     C_{m_{ac}} : 2.388   
-        \bar{c} : 1.388   
-            S_w : 1       
+            w_f : 0.7264  
+            S_w : 0.6368  
+     C_{L_{aw}} : 0.6368  
+            x_w : 0.566   
+            K_f : 0.3632  
 \tan(\Lambda_h) : 0.1532  
+        \bar{c} : 0.07075 
+     S.M._{min} : 0.07075 
            AR_h : -0.3872 
-            x_w : -0.3879 
            \eta : -0.6128 
-       l_{fuse} : -1      
-     C_{L_{aw}} : -6.164  
-   \alpha_{max} : -7.164  
+       l_{fuse} : -0.6368 
 
