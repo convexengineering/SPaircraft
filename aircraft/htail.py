403b0896b4b57b8946196a43e6724dbe8909a2d5
pgkirsch <kirschen@mit.edu>
2016-03-21 13:55:38 -0400

    added var descrs

--- aircraft/htail.py
+++ aircraft/htail.py
@@ -2,28 +2,31 @@ from gpkit import Variable, Model, SignomialsEnabled, units
 from gpkit.constraints.tight import TightConstraintSet as TCS
 from numpy import pi, tan
 
-alpha = Variable('\\alpha', '-')
-ARh  = Variable('AR_h', '-')
-amax = Variable('\\alpha_{max}', '-')
-eta  = Variable('\\eta', '-')
-CLah = Variable('C_{L_{ah}}', '-')
-CLaw = Variable('C_{L_{aw}}', '-')
-CLh  = Variable('C_{L_h}', '-')
-CLw  = Variable('C_{L_w}', '-')
-cma  = Variable('\\bar{c}', 'm')
-Cmac = Variable('C_{m_{ac}}', '-')
-Kf   = Variable('K_f', '-')
-lh   = Variable('l_h', 'm')
-lfuse= Variable('l_{fuse}', 'm')
-Sh   = Variable('S_h', 'm^2')
-Sw   = Variable('S_w', 'm^2')
-SM   = Variable('S.M.', '-')
-SMmin= Variable('S.M._{min}', '-')
-tanLh= Variable('\\tan(\\Lambda_h)', '-')
-xCG  = Variable('x_{CG}', 'm')
-xw   = Variable('x_w', 'm')
-W    = Variable('W', 'N')
-wf   = Variable('w_f', 'm')
+alpha = Variable('\\alpha', '-', 'Horizontal tail angle of attack')
+ARh  = Variable('AR_h', '-', 'Horizontal tail aspect ratio')
+amax = Variable('\\alpha_{max}', '-', 'Max angle of attack (htail)')
+CLah = Variable('C_{L_{ah}}', '-', 'Lift curve slope (htail)')
+CLaw = Variable('C_{L_{aw}}', '-', 'Lift curve slope (wing)')
+CLh  = Variable('C_{L_h}', '-', 'Lift coefficient (htail)')
+CLw  = Variable('C_{L_w}', '-', 'Lift coefficient (wing)')
+cma  = Variable('\\bar{c}', 'm', 'Mean aerodynamic chord (wing)')
+Cmac = Variable('C_{m_{ac}}', '-',
+                'Moment coefficient about aerodynamic centre (wing)')
+Cmfu = Variable('C_{m_{fuse}}', '-', 'Moment coefficient (fuselage)')
+eta  = Variable('\\eta', '-',
+                'Lift efficiency (diff between sectional and actual lift)')
+Kf   = Variable('K_f', '-', 'Empirical factor for fuselage-wing interference')
+lh   = Variable('l_h', 'm', 'Horizontal tail moment arm')
+lfuse= Variable('l_{fuse}', 'm', 'Fuselage length')
+Sh   = Variable('S_h', 'm^2', 'Horizontal tail area')
+Sw   = Variable('S_w', 'm^2', 'Wing area')
+SM   = Variable('S.M.', '-', 'Stability margin')
+SMmin= Variable('S.M._{min}', '-', 'Minimum stability margin')
+tanLh= Variable('\\tan(\\Lambda_h)', '-', 'tangent of horizontal tail sweep')
+xCG  = Variable('x_{CG}', 'm', 'CG location')
+xw   = Variable('x_w', 'm', 'Distance from aerodynamic centre to CG')
+W    = Variable('W', 'N', 'Horizontal tail weight')
+wf   = Variable('w_f', 'm', 'Fuselage width')
 
 
 objective = W
@@ -36,7 +39,7 @@ with SignomialsEnabled():
                    SM >= SMmin,
 
                    # Trim
-                   CLh*Sh*lh/(Sw*cma) >= Cmac + CLw*xw/cma,
+                   CLh*Sh*lh/(Sw*cma) >= Cmac + CLw*xw/cma + Cmfu,
 
                    TCS([xCG + lh <= lfuse]),
 
@@ -53,9 +56,10 @@ substitutions = {
                  '\\alpha_{max}': 0.1, # (6 deg)
                  'AR_h': 4,
                  '\\eta': 0.97,
-                 'C_{m_{ac}}': 0.1,
                  'C_{L_{aw}}': 2*pi,
                  'C_{L_w}': 0.5,
+                 'C_{m_{ac}}': 0.1,
+                 'C_{m_{fuse}}': 0.1,
                  '\\bar{c}': 5,
                  'K_f': 0.7,
                  'l_{fuse}': 40,
--- aircraft/htail.sol
+++ aircraft/htail.sol
@@ -1,6 +1,6 @@
 Using solver 'mosek'
 Solving for 7 variables.
-Solving took 0.0167 seconds.
+Solving took 0.0171 seconds.
 
 Cost
 ----
@@ -8,43 +8,44 @@ Cost
 
 Free Variables
 --------------
-   C_{L_h} : 0.3508         
-C_{L_{ah}} : 3.508          
-      S.M. : 0.05           
-       S_h : 39.56   [m**2] 
-         W : 3956    [N]    
-    \alpha : 0.1            
-       l_h : 20      [m]    
+   C_{L_h} : 0.3508         Lift coefficient (htail)       
+C_{L_{ah}} : 3.508          Lift curve slope (htail)       
+      S.M. : 0.05           Stability margin               
+       S_h : 39.56   [m**2] Horizontal tail area           
+         W : 3956    [N]    Horizontal tail weight         
+    \alpha : 0.1            Horizontal tail angle of attack
+       l_h : 20      [m]    Horizontal tail moment arm     
 
 Constants
 ---------
-           AR_h : 4              
-        C_{L_w} : 0.5            
-     C_{L_{aw}} : 6.283          
-     C_{m_{ac}} : 0.1            
-            K_f : 0.7            
-     S.M._{min} : 0.05           
-            S_w : 125     [m**2] 
-   \alpha_{max} : 0.1            
-        \bar{c} : 5       [m]    
-           \eta : 0.97           
-\tan(\Lambda_h) : 0.5774         
-       l_{fuse} : 40      [m]    
-            w_f : 6       [m]    
-            x_w : 2       [m]    
-         x_{CG} : 20      [m]    
+           AR_h : 4              Horizontal tail aspect ratio                            
+        C_{L_w} : 0.5            Lift coefficient (wing)                                 
+     C_{L_{aw}} : 6.283          Lift curve slope (wing)                                 
+     C_{m_{ac}} : 0.1            Moment coefficient about aerodynamic centre (wing)      
+   C_{m_{fuse}} : 0.1            Moment coefficient (fuselage)                           
+            K_f : 0.7            Empirical factor for fuselage-wing interference         
+     S.M._{min} : 0.05           Minimum stability margin                                
+            S_w : 125     [m**2] Wing area                                               
+   \alpha_{max} : 0.1            Max angle of attack (htail)                             
+        \bar{c} : 5       [m]    Mean aerodynamic chord (wing)                           
+           \eta : 0.97           Lift efficiency (diff between sectional and actual lift)
+\tan(\Lambda_h) : 0.5774         tangent of horizontal tail sweep                        
+       l_{fuse} : 40      [m]    Fuselage length                                         
+            w_f : 6       [m]    Fuselage width                                          
+            x_w : 2       [m]    Distance from aerodynamic centre to CG                  
+         x_{CG} : 20      [m]    CG location                                             
 
 Sensitivities
 -------------
-            w_f : 0.7264  
-            S_w : 0.6368  
-     C_{L_{aw}} : 0.6368  
-            x_w : 0.566   
-            K_f : 0.3632  
-\tan(\Lambda_h) : 0.1532  
-        \bar{c} : 0.07075 
-     S.M._{min} : 0.07075 
-           AR_h : -0.3872 
-           \eta : -0.6128 
-       l_{fuse} : -0.6368 
+            w_f : 0.7264  Fuselage width                                          
+            S_w : 0.6368  Wing area                                               
+     C_{L_{aw}} : 0.6368  Lift curve slope (wing)                                 
+            x_w : 0.566   Distance from aerodynamic centre to CG                  
+            K_f : 0.3632  Empirical factor for fuselage-wing interference         
+\tan(\Lambda_h) : 0.1532  tangent of horizontal tail sweep                        
+        \bar{c} : 0.07075 Mean aerodynamic chord (wing)                           
+     S.M._{min} : 0.07075 Minimum stability margin                                
+           AR_h : -0.3872 Horizontal tail aspect ratio                            
+           \eta : -0.6128 Lift efficiency (diff between sectional and actual lift)
+       l_{fuse} : -0.6368 Fuselage length                                         
 
