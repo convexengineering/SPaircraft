6398d077c1033f7aeb8259b094ebce90c4d21c7c
pgkirsch <kirschen@mit.edu>
2016-04-14 20:00:51 -0400

    fixed CLwmax error, add aircraft classmethod for wing

--- gpkitmodels/aircraft/wing.py
+++ gpkitmodels/aircraft/wing.py
@@ -10,36 +10,35 @@ class Wing(CostedConstraintSet):
     Wing sizing
     """
     def __init__(self, **kwargs):
-        alpha   = Variable('\\alpha', '-', 'Wing angle of attack')
+        alpha   = Variable('\\alpha_w', '-', 'Wing angle of attack')
         AR      = Variable('AR', '-', 'Wing aspect ratio')
-        amax    = Variable('\\alpha_{max}', '-', 'Max angle of attack')
+        amax    = Variable('\\alpha_{max,w}', '-', 'Max angle of attack')
         b       = Variable('b_w', 'm', 'Wing span')
         CDw     = Variable('C_{D_w}', '-', 'Drag coefficient')
         CD0w    = Variable('C_{D_{0_w}}', '-',
                            'Wing parasitic drag coefficient')
         CLaw    = Variable('C_{L_{aw}}', '-', 'Lift curve slope (wing)')
         CLw     = Variable('C_{L_w}', '-', 'Lift coefficient (wing)')
-        CLwmax  = Variable('C_{L_w}', '-', 'Lift coefficient (wing)')
+        CLwmax  = Variable('C_{L_{wmax}}', '-', 'Lift coefficient (wing)')
         croot   = Variable('c_{root}', 'm', 'Wing root chord')
         ctip    = Variable('c_{tip}', 'm', 'Wing tip chord')
         cwma    = Variable('\\bar{c}_{wing}', 'm', 'Mean aerodynamic chord (wing)')
-        D       = Variable('D_h', 'N', 'Wing drag')
-        e       = Variable('e', '-', 'Oswald efficiency factor')
-        eta     = Variable('\\eta', '-',
+        D       = Variable('D_w', 'N', 'Wing drag')
+        e       = Variable('e_w', '-', 'Oswald efficiency factor')
+        eta     = Variable('\\eta_w', '-',
                            'Lift efficiency (diff between sectional and actual lift)')
-        fl      = Variable('f\(\\lambda\)', '-', 'Empirical efficiency function of taper')
-        L       = Variable('L', 'N', 'Lift')
-        Lmax    = Variable('L_{max}', 'N', 'Maximum load')
+        fl      = Variable('f(\\lambda_w)', '-', 'Empirical efficiency function of taper')
+        Lmax    = Variable('L_{max_{w}}', 'N', 'Maximum load')
         mu      = Variable('\\mu', 'N*s/m^2', 'Dynamic viscosity (35,000ft)')
-        p       = Variable('p', '-', 'Substituted variable = 1 + 2*taper')
-        q       = Variable('q', '-', 'Substituted variable = 1 + taper')
-        Rec     = Variable('Re_c', '-', 'Cruise Reynolds number (Wing)')
+        p       = Variable('p_w', '-', 'Substituted variable = 1 + 2*taper')
+        q       = Variable('q_w', '-', 'Substituted variable = 1 + taper')
+        Rec     = Variable('Re_w', '-', 'Cruise Reynolds number (Wing)')
         rho     = Variable('\\rho', 'kg/m^3', 'Air density (35,000 ft)')
         rho0    = Variable('\\rho_0', 'kg/m^3', 'Air density (0 ft)')
         Sw      = Variable('S_w', 'm^2', 'Wing area')
         tanL    = Variable('\\tan(\\Lambda)', '-', 'tangent of wing sweep')
         taper   = Variable('\\lambda', '-', 'Wing taper ratio')
-        tau     = Variable('\\tau', '-', 'Wing thickness/chord ratio')
+        tau     = Variable('\\tau_w', '-', 'Wing thickness/chord ratio')
         Vinf    = Variable('V_{\\infty}', 'm/s', 'Freestream velocity')
         Vne     = Variable('V_{ne}', 'm/s', 'Never exceed velocity')
         W       = Variable('W', 'N', 'Aircraft weight')
@@ -53,9 +52,7 @@ class Wing(CostedConstraintSet):
 
         with SignomialsEnabled():
             constraints = [
-                           L == W,
-                           L == 0.5*rho*Vinf**2*Sw*CLw,
-                           W >= W0 + Ww,
+                           W == 0.5*rho*Vinf**2*Sw*CLw,
 
         #                   p >= 1 + 2*taper,
         #                   2*q >= 1 + p,
@@ -79,12 +76,15 @@ class Wing(CostedConstraintSet):
                            # Nita, Scholz, "Estimating the Oswald factor from basic
                            # aircraft geometrical parameters"
                            TCS([fl >= 0.0524*taper**4 - 0.15*taper**3 + 0.1659*taper**2
-                                    - 0.0706*taper + 0.0119], reltol=1E-5),
+                                    - 0.0706*taper + 0.0119], reltol=1E-2),
                            TCS([e*(1 + fl*AR) <= 1]),
                            taper >= 0.2, # TODO
 
                            Lmax == 0.5*rho0*Vne**2*Sw*CLwmax,
                           ]
+
+            standalone_constraints = [W >= W0 + Ww,]
+            self.standalone_constraints = standalone_constraints
 
         wb = WingBox()
         wb.subinplace({'A': AR,
@@ -105,10 +105,13 @@ class Wing(CostedConstraintSet):
     def standalone_737(cls):
         ccs = cls()
 
+        constraints = ccs + ccs.standalone_constraints
+
         substitutions = {
-                         '\\alpha_{max}': 0.1, # (6 deg)
-                         '\\eta': 0.97,
+                         '\\alpha_{max,w}': 0.1, # (6 deg)
+                         '\\eta_w': 0.97,
                          'C_{D_{0_w}}': 0.05,
+                         'C_{L_{wmax}}': 2.5,
                          '\\mu': 1.4E-5,
                          '\\rho': 0.38,
                          '\\rho_0': 1.225,
@@ -116,6 +119,25 @@ class Wing(CostedConstraintSet):
                          'V_{\\infty}': 240,
                          'V_{ne}': 144,
                          'W_0': 1E5,
+                        }
+
+        m = Model(ccs.cost, constraints, substitutions)
+        return m
+
+    @classmethod
+    def aircraft_737(cls):
+        ccs = cls()
+
+        substitutions = {
+                         '\\alpha_{max,w}': 0.1, # (6 deg)
+                         '\\eta_w': 0.97,
+                         'C_{D_{0_w}}': 0.05,
+                         'C_{L_{wmax}}': 2.5,
+                         '\\mu': 1.4E-5,
+                         '\\rho': 0.38,
+                         '\\rho_0': 1.225,
+                         '\\tan(\\Lambda)': tan(30*pi/180),
+                         'V_{ne}': 144,
                         }
 
         m = Model(ccs.cost, ccs, substitutions)
--- gpkitmodels/aircraft/wing.sol
+++ gpkitmodels/aircraft/wing.sol
@@ -1,50 +1,50 @@
 Beginning signomial solve.
-Solving took 5 GP solves and 2.97 seconds.
+Solving took 4 GP solves and 1.49 seconds.
 
 Cost
 ----
- 1.389e+04 [N] 
+ 1.954e+04 [N] 
 
 Free Variables
 --------------
-          AR : 12.04             Wing aspect ratio                      
-     C_{D_w} : 0.0558            Drag coefficient                       
-     C_{L_w} : 0.4593            Lift coefficient (wing)                
-  C_{L_{aw}} : 4.593             Lift curve slope (wing)                
-         D_h : 1.249e+04  [N]    Wing drag                              
-           L : 1.028e+05  [N]    Lift                                   
-     L_{max} : 1.193e+05  [N]    Maximum load                           
-         S_w : 20.45      [m**2] Wing area                              
-           W : 1.028e+05  [N]    Aircraft weight                        
-         W_w : 2800       [N]    Wing weight                            
-      \alpha : 0.1               Wing angle of attack                   
+          AR : 5.776             Wing aspect ratio                      
+     C_{D_w} : 0.05881           Drag coefficient                       
+     C_{L_w} : 0.3962            Lift coefficient (wing)                
+  C_{L_{aw}} : 3.962             Lift curve slope (wing)                
+         D_w : 1.592e+04  [N]    Wing drag                              
+ L_{max_{w}} : 7.854e+05  [N]    Maximum load                           
+         S_w : 24.74      [m**2] Wing area                              
+           W : 1.072e+05  [N]    Aircraft weight                        
+         W_w : 7243       [N]    Wing weight                            
+    \alpha_w : 0.1               Wing angle of attack                   
      \lambda : 0.2               Wing taper ratio                       
-        \tau : 0.15              Wing thickness/chord ratio             
-         b_w : 15.69      [m]    Wing span                              
-    c_{root} : 2.172      [m]    Wing root chord                        
-     c_{tip} : 0.4345     [m]    Wing tip chord                         
-           e : 0.9618            Oswald efficiency factor               
-f\(\lambda\) : 0.0033            Empirical efficiency function of taper 
-           p : 1.4               Substituted variable = 1 + 2*taper     
-           q : 1.2               Substituted variable = 1 + taper       
+      \tau_w : 0.15              Wing thickness/chord ratio             
+         b_w : 11.95      [m]    Wing span                              
+    c_{root} : 3.449      [m]    Wing root chord                        
+     c_{tip} : 0.6898     [m]    Wing tip chord                         
+         e_w : 0.9812            Oswald efficiency factor               
+f(\lambda_w) : 0.003309          Empirical efficiency function of taper 
+         p_w : 1.4               Substituted variable = 1 + 2*taper     
+         q_w : 1.2               Substituted variable = 1 + taper       
                                                                         
      WingBox |                                                          
-     I_{cap} : 1.065e-05         Non-dim spar cap area moment of inertia
-         M_r : 8.379e+04  [N]    Root moment per root chord             
-     W_{cap} : 1877       [N]    Weight of spar caps                    
-     W_{web} : 122.7      [N]    Weight of shear web                    
+     I_{cap} : 1.335e-05         Non-dim spar cap area moment of inertia
+         M_r : 2.646e+05  [N]    Root moment per root chord             
+     W_{cap} : 4558       [N]    Weight of spar caps                    
+     W_{web} : 615.5      [N]    Weight of shear web                    
          \nu : 0.8612            Dummy variable = $(t^2 + t + 1)/(t+1)$ 
-     t_{cap} : 0.002315          Non-dim. spar cap thickness            
-     t_{web} : 0.0006728         Non-dim. shear web thickness           
+     t_{cap} : 0.002928          Non-dim. spar cap thickness            
+     t_{web} : 0.001757          Non-dim. shear web thickness           
 
 Constants
 ---------
        C_{D_{0_w}} : 0.05                Wing parasitic drag coefficient                         
+      C_{L_{wmax}} : 2.5                 Lift coefficient (wing)                                 
         V_{\infty} : 240       [m/s]     Freestream velocity                                     
             V_{ne} : 144       [m/s]     Never exceed velocity                                   
                W_0 : 1e+05     [N]       Weight excluding wing                                   
-      \alpha_{max} : 0.1                 Max angle of attack                                     
-              \eta : 0.97                Lift efficiency (diff between sectional and actual lift)
+    \alpha_{max,w} : 0.1                 Max angle of attack                                     
+            \eta_w : 0.97                Lift efficiency (diff between sectional and actual lift)
               \rho : 0.38      [kg/m**3] Air density (35,000 ft)                                 
             \rho_0 : 1.225     [kg/m**3] Air density (0 ft)                                      
      \tan(\Lambda) : 0.5774              tangent of wing sweep                                   
@@ -62,20 +62,25 @@ Constants
 
 Sensitivities
 -------------
-      WingBox |                                                                 
-     N_{lift} : 0.135   Wing loading multiplier                                 
-            g : 0.1306  Gravitational acceleration                              
-   \rho_{cap} : 0.1226  Density of spar cap material                            
-    f_{w,add} : 0.03732 Wing added weight fraction                              
- \sigma_{max} : -0.127  Allowable tensile stress                                
-                                                                                
-          W_0 : 1.065   Weight excluding wing                                   
-  C_{D_{0_w}} : 0.8058  Wing parasitic drag coefficient                         
-       V_{ne} : 0.2701  Never exceed velocity                                   
-\tan(\Lambda) : 0.1666  tangent of wing sweep                                   
-       \rho_0 : 0.135   Air density (0 ft)                                      
-         \rho : -0.1959 Air density (35,000 ft)                                 
-   V_{\infty} : -0.3919 Freestream velocity                                     
-         \eta : -0.6664 Lift efficiency (diff between sectional and actual lift)
- \alpha_{max} : -0.7732 Max angle of attack                                     
+           WingBox |                                                                  
+          N_{lift} : 0.2784   Wing loading multiplier                                 
+                 g : 0.2674   Gravitational acceleration                              
+        \rho_{cap} : 0.2356   Density of spar cap material                            
+         f_{w,add} : 0.07641  Wing added weight fraction                              
+               r_h : 0.03182  Fractional wing thickness at spar web                   
+        \rho_{web} : 0.03182  Density of shear web material                           
+                 w : -0.01092 Wingbox-width-to-chord ratio                            
+\sigma_{max,shear} : -0.03182 Allowable shear stress                                  
+      \sigma_{max} : -0.2465  Allowable tensile stress                                
+                                                                                      
+               W_0 : 1.134    Weight excluding wing                                   
+       C_{D_{0_w}} : 0.6926   Wing parasitic drag coefficient                         
+            V_{ne} : 0.5567   Never exceed velocity                                   
+            \rho_0 : 0.2784   Air density (0 ft)                                      
+      C_{L_{wmax}} : 0.2784   Lift coefficient (wing)                                 
+     \tan(\Lambda) : 0.1751   tangent of wing sweep                                   
+              \rho : -0.4012  Air density (35,000 ft)                                 
+            \eta_w : -0.7003  Lift efficiency (diff between sectional and actual lift)
+        V_{\infty} : -0.8023  Freestream velocity                                     
+    \alpha_{max,w} : -0.9716  Max angle of attack                                     
 
