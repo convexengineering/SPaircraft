da11e1378781904f055e6dd766850277d14c7c06
mjburton <mjburton@mit.edu>
2017-02-22 18:20:28 -0500

    initial wing gp

--- gpmodel/gp_d8.py
+++ gpmodel/gp_d8.py
@@ -98,6 +98,7 @@ class FlightState(Model):
         Tatm = Variable("T_{atm}", "K", "air temperature")
         Tsl = Variable("T_{sl}", 288.15, "K", "sea level temperature")
         rhosl = Variable('\\rho_{sl}', 1.225, 'kg/m^3', 'Density of air')
+        mu = Variable("\\mu", 1.45e-5, "kg/m/s", "dynamic viscosity")
 
         constraints = [a == (gamma*Rspec*Tatm)**.5,
                        V == m*a,
@@ -126,7 +127,7 @@ class Wing(Model):
 
     def dynamic(self, state):
         " wing aero "
-        return WingPerf(self, state)
+        return WingPerfMach(self, state)
 
 class WingRaymer(Model):
     """
@@ -153,9 +154,81 @@ class WingRaymer(Model):
 
     def dynamic(self, state):
         " wing aero "
-        return WingPerf(self, state)
+        return WingPerfMach(self, state)
 
-class WingPerf(Model):
+class WingBox(Model):
+    """
+    Structural model for a wing
+    source: Hoburg, "Geometric Programming for Aircraft Design Optimization"
+    Note - does not have a performance model
+    """
+    def setup(self):
+
+        Icap = Variable('I_{cap}', '-',
+                        'Non-dim spar cap area moment of inertia')
+        Mr = Variable('M_r', 'N', 'Root moment per root chord')
+        nu = Variable('\\nu', '-',
+                      'Dummy variable = $(t^2 + t + 1)/(t+1)$')
+        Wcap = Variable('W_{cap}', 'N', 'Weight of spar caps')
+        Wweb = Variable('W_{web}', 'N', 'Weight of shear web')
+        Wstruct = Variable('W', 'N', 'Structural weight')
+        taper = Variable('taper', 0.5, '-', 'Taper ratio')
+        fwadd = Variable('f_{w,add}', 0.4, '-', 'Wing added weight fraction')
+        g = Variable('g', 9.81, 'm/s^2', 'Gravitational acceleration')
+        Nlift = Variable('N_{lift}', 2.0, '-', 'Wing loading multiplier')
+        rh = Variable('r_h', 0.75, '-',
+                      'Fractional wing thickness at spar web')
+        rhocap = Variable('\\rho_{cap}', 2700, 'kg/m^3',
+                          'Density of spar cap material')
+        rhoweb = Variable('\\rho_{web}', 2700, 'kg/m^3',
+                          'Density of shear web material')
+        sigmax = Variable('\\sigma_{max}', 250e6, 'Pa',
+                          'Allowable tensile stress')
+        sigmaxshear = Variable('\\sigma_{max,shear}', 167e6, 'Pa',
+                               'Allowable shear stress')
+        wwb = Variable('wwb', 0.5, '-', 'Wingbox-width-to-chord ratio')
+        tcap = Variable('t_{cap}', '-', 'Non-dim. spar cap thickness')
+        tweb = Variable('t_{web}', '-', 'Non-dim. shear web thickness')
+        p = Variable('p', '-', 'Substituted variable = 1 + 2*taper')
+        q = Variable('q', '-', 'Substituted variable = 1 + taper')
+        ymac = Variable('y_{mac}', 'm',
+                        'Spanwise location of mean aerodynamic chord')
+        rho0 = Variable('\\rho_0', 1.225, 'kg/m^3', 'Air density (0 ft)')
+        AR = Variable('AR', '-', 'Wing aspect ratio')
+        Lmax = Variable('L_{max}', 'N', 'Maximum load')
+        Sw = Variable('S', 'm^2', 'Wing area')
+        Vne = Variable('V_{ne}', 60, 'm/s', 'Never exceed velocity')
+        b = Variable('b', 'm', 'Wing span')
+        CLwmax = Variable('C_{L_{wmax}}', 1.2, '-', 'Max lift coefficient, wing')
+        tau = Variable('\\tau', 0.15, '-', 'Wing thickness/chord ratio')
+        mac = Variable('mac', 'm', 'Mean aerodynamic chord (wing)')
+        e = Variable('e', 0.9, '-', 'Oswald efficiency factor')
+
+        constraints = [
+            AR == b**2/Sw,
+            mac == Sw/b,
+            p >= 1 + 2*taper,
+            2*q >= 1 + p,
+            ymac == (b/3)*q/p,
+            Lmax == 0.5*rho0*Vne**2*Sw*CLwmax,
+            Mr >= Lmax*AR*p/24,
+            0.92*wwb*tau*tcap**2 + Icap <= 0.92**2/2*wwb*tau**2*tcap,
+            8 >= Nlift*Mr*AR*q**2*tau/(Sw*Icap*sigmax),
+            12 >= AR*Lmax*Nlift*q**2/(tau*Sw*tweb*sigmaxshear),
+            nu**3.94 >= 0.86*p**(-2.38)+ 0.14*p**0.56,
+            Wcap >= 8*rhocap*g*wwb*tcap*Sw**1.5*nu/(3*AR**0.5),
+            TCS([Wweb >= 8*rhoweb*g*rh*tau*tweb*Sw**1.5*nu/(3*AR**0.5)]),
+            Wstruct >= (1 + fwadd)*(Wweb + Wcap),
+            ]
+
+        return constraints
+
+    def dynamic(self, state):
+        " wing aero "
+        return WingPerfRe(self, state)
+
+
+class WingPerfMach(Model):
     " wing aero "
     def setup(self, wing, state):
 
@@ -177,6 +250,36 @@ class WingPerf(Model):
                 * state['M']**146.51920742858428),
             Dwing >= ((0.5*wing['S']*state['\\rho']*state['V']**2)
                       * (Cdw + wing['K']*CL**2)),
+            ]
+
+        return constraints
+
+class WingPerfRe(Model):
+    """
+    Wing performance model
+    """
+    def setup(self, wing, state):
+        self.wing = wing
+
+        Re = Variable('Re_w', '-', 'Reynolds number (wing)')
+        CDp = Variable('C_{D_{p_w}}', '-',
+                       'Wing parasitic drag coefficient')
+        CDw = Variable('C_{d_w}', '-', 'Drag coefficient, wing')
+        CLw = Variable('C_{L}', '-', 'Lift coefficient, wing')
+        D = Variable('D_{wing}', 'N', 'Wing drag')
+        Lw = Variable('L_w', 'N', 'Wing lift')
+
+        constraints = [
+            Lw == 0.5*state['\\rho']*state['V']**2*self.wing['S']*CLw,
+            D == 0.5*state['\\rho']*state['V']**2*self.wing['S']*CDw,
+            CDw >= CDp + CLw**2/(np.pi*self.wing['e']*self.wing['AR']),
+            Re == state['\\rho']*state['V']*self.wing['mac']/state['\\mu'],
+            1 >= (
+                2.56*CLw**5.88/(Re**1.54*self.wing['\\tau']**3.32*CDp**2.62)
+                +3.8e-9*self.wing['\\tau']**6.23/(CLw**0.92*Re**1.38*CDp**9.57)
+                +2.2e-3*Re**0.14*self.wing['\\tau']**0.033/(CLw**0.01*CDp**0.73)
+                +6.14e-6*CLw**6.53/(Re**0.99*self.wing['\\tau']**0.52*CDp**5.19)
+                +1.19e4*CLw**9.78*self.wing['\\tau']**1.76/(Re*CDp**0.91)),
             ]
 
         return constraints
@@ -216,11 +319,13 @@ def subbing(model, substitutions):
 def init_subs(model):
     " initialize substitutions "
     subs = {"W_{payload}": 1000, "f_{struct}": 0.5, "V_{stall}": 120,
-            "TSFC": 0.7, "R": 1000, "AR": 15, "e": 0.9, "CDA": 0.1}
+            "TSFC": 0.7, "R": 1000, "AR": 15, "e": 0.9, "CDA": 0.1,
+            "\\rho": 0.4}
     subbing(model, subs)
 
 if __name__ == "__main__":
-    wing = Wing()
+    # wing = Wing()
+    wing = WingBox()
     aircraft = Aircraft(wing)
     M = Mission(aircraft)
     init_subs(M)
