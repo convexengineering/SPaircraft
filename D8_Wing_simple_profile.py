d2344bc736c041c681d8898629d0f3747a815a80
1ozturkbe <1ozturkbe@gmail.com>
2017-03-10 15:55:48 -0500

    New nu fit well behaved for all but VT.

--- D8_Wing_simple_profile.py
+++ D8_Wing_simple_profile.py
@@ -480,8 +480,8 @@ class WingBox(Model):
                        TCS([12 >= AR*Lmax*q**2/(tau*S*tweb*sigmaxshear)]),
 
                        # Posynomial approximation of nu=(1+lam+lam^2)/(1+lam)
-                       nu**3.94 >= 0.86*p**(-2.38)+ 0.14*p**0.56, # PHILIPPE'S FIT
-                       #  (nu/1.09074074)**.166 >= 0.205*(p/1.7)**0.772 + 0.795*(p/1.7)**-0.125, # BERK'S FIT
+                       # nu**3.94 >= 0.86*p**(-2.38)+ 0.14*p**0.56, # PHILIPPE'S FIT
+                        (nu/1.09074074)**.166 >= 0.205*(p/1.7)**0.772 + 0.795*(p/1.7)**-0.125, # BERK'S FIT
 
 
                        # Weight of spar caps and shear webs
