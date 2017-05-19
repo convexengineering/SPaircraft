09543890d9288ba6ac1ec96d66040f6703a07d90
mayork <mayork@mit.edu>
2017-05-19 07:58:38 -0400

    adding file to post compute things like propulsive efficiency

---
+++ post_compute.py
@@ -0,0 +1,12 @@
+"""
+function to compute aircraft values after solving model
+"""
+
+from gpkit.small_scripts import mag
+
+def post_compute(sol, Nclimb):
+    eta_P_fan = 2/(1 + 1.94384*mag(sol('u_8')[Nclimb][0])/sol('V')['V_Mission, FlightState'][Nclimb][0])
+
+    print "Fan Propuslive Efficiency in Cruise Segment 1"
+    print "---------------------"
+    print eta_P_fan
