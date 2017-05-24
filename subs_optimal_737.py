401e01ffc60e8f6cf7e6d2ba275591791a38e35f
mayork <mayork@mit.edu>
2017-05-24 14:39:29 -0400

    adding max climb distance/time constriants

--- subs_optimal_737.py
+++ subs_optimal_737.py
@@ -187,6 +187,10 @@ def get737_optimal_subs():
             # nacelle drag calc parameter
             'r_{vnace}': 1.02,
             'T_{t_{4.1_{max}}}': 1567.*units('K'),
+
+            'MaxClimbTime': 20*units('min'),
+##            'MaxClimbDistance': 180*units('nautical_miles')
+            
     }
 
     return substitutions
