90f488f170494d99339304239c864275700c8ab1
mayork <mayork@mit.edu>
2017-08-22 10:00:09 -0500

    giving test function required arguments

--- SP_aircraft.py
+++ SP_aircraft.py
@@ -1402,7 +1402,7 @@ def run_small_M08_D8_no_BLI(objective, fixedBPR, pRatOpt = False):
     return sol
 
 def test():
-    run_optimal_737()
+    run_optimal_737('W_{f_{total}}', True, False)
 
 if __name__ == '__main__':
     # User definitions
