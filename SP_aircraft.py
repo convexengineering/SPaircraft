3f18971dd2f3a2e1f65c8e582c87978d71c783a9
mayork <mayork@mit.edu>
2017-11-22 11:37:51 -0500

    adding new reqired arugment to test function

--- SP_aircraft.py
+++ SP_aircraft.py
@@ -517,7 +517,7 @@ def run_D8_no_BLI(objective, fixedBPR, pRatOpt, mutategparg):
     return sol
 
 def test():
-    run_optimal_737('W_{f_{total}}', True, False)
+    run_optimal_737('W_{f_{total}}', True, False, True)
 
 ##    OLD CODE DOESN'T CURRENTLY WORK
     
